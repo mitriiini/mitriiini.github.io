bc4939b200d45afb10d0814064794238e96c96c6
mitriiini <mitriiinistudio@gmail.com>
2025-08-25 17:56:45 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-// consent.js
+// auth.js
 document.addEventListener('DOMContentLoaded', () => {
     // Créez le conteneur pour le pop-up et l'overlay
     const consentOverlay = document.createElement('div');
@@ -16,11 +16,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.body.appendChild(consentOverlay);
 
-    // Sélectionnez les boutons et l'élément audio
+    // Sélectionnez les boutons, l'élément audio et le contenu de la page
+    const pageContent = document.getElementById('page-content');
     const acceptButton = consentOverlay.querySelector('.accept-btn');
     const refuseButton = consentOverlay.querySelector('.refuse-btn');
     // Le chemin vers votre fichier audio
-    const audioFilePath = 'audio/test.mp3'; 
+    const audioFilePath = 'audio/test.mp3';
     const backgroundAudio = new Audio(audioFilePath);
     backgroundAudio.volume = 0; // Commence avec un volume de 0
     let intervalId = null;
@@ -54,15 +55,31 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }, 100); // Règle la vitesse du fondu
     }
-    
-    // Cache le pop-up et joue le son si l'utilisateur accepte
+
+    // Fonction pour cacher l'overlay avec un effet de transition et afficher le contenu
+    function hideOverlayAndShowContent() {
+        // Applique les classes CSS de fondu et de flou à l'overlay
+        consentOverlay.classList.add('fade-out');
+        consentOverlay.classList.add('blur-out');
+
+        // Attend la fin de la transition pour masquer l'élément complètement
+        consentOverlay.addEventListener('transitionend', () => {
+            consentOverlay.classList.add('hidden');
+            // Affiche le contenu de la page après la disparition de l'overlay
+            pageContent.classList.remove('hidden-content');
+            pageContent.classList.add('visible-content');
+        }, {
+            once: true
+        });
+    }
+
+    // Gère le clic sur le bouton "Accepter"
     acceptButton.addEventListener('click', () => {
-        consentOverlay.classList.add('hidden');
-        
+        hideOverlayAndShowContent();
         // Joue l'audio et lance le fondu entrant
         backgroundAudio.play().then(() => {
             fadeIn();
-            
+
             // Surveille le temps restant pour relancer l'audio
             intervalId = setInterval(() => {
                 const timeRemaining = backgroundAudio.duration - backgroundAudio.currentTime;
@@ -73,9 +90,9 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // Cache le pop-up si l'utilisateur refuse
+    // Gère le clic sur le bouton "Refuser"
     refuseButton.addEventListener('click', () => {
-        consentOverlay.classList.add('hidden');
+        hideOverlayAndShowContent();
     });
 
     // Nettoyage de l'intervalle si la page est quittée
